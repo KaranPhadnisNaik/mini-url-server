d6a4a56f3446a7f2798ab8eefca11a97095dc6b1
karanphadnisnaik <Karanphadnis1@gmail.com>
2017-09-10 18:32:50 -0700

    change db setup

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:/
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'karan'
 api = Api(app)
-'''
-@app.before_first_request
-def create_tables():
-    db.create_all()
-'''
+
+#@app.before_first_request
+#def create_tables():
+#    db.create_all()
+
 
 jwt = JWT(app, authenticate,identity) # creates a new endpoint /auth,
                                       # sends username and password to authnetiate function
